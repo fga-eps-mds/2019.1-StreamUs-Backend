79cc5f340765702caeaf53decf46c18c76e716fc
lucas <l.lopes.fga@gmail.com>
2019-04-13 18:55:44 -0300

    Create a function to receive a new request by refresh the token

--- src/api/login-rest.js
+++ src/api/login-rest.js
@@ -107,3 +107,30 @@ app.use(express.static(__dirname + '/public'))
     }
   });
   
+
+  app.get('/refresh_token', function(req, res) {
+    
+    //requisição de acesso pelo novo token
+
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      },
+      json: true
+    };
+  
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token;
+        console.log(access_token)
+        res.send({
+          'access_token': access_token
+        });
+      }
+    });
+  });
+  
