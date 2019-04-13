54352f5cfe633da53f5f1f7881a5fa422f51eb7d
lucas <l.lopes.fga@gmail.com>
2019-04-13 18:31:03 -0300

    Função geradora de string aleatoria

--- src/api/login-rest.js
+++ src/api/login-rest.js
@@ -7,3 +7,15 @@ const cookieParser = require('cookie-parser');
 var client_id = 'be628abe9b224f61b5777fb3d0d6a636'; 
 var client_secret = 'df77974aade94da8a62a76af55ad9825'; 
 var redirect_uri = 'http://localhost:8888/callback/'; 
+
+
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+  
