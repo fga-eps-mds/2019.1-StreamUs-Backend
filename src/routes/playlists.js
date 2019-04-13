47ef9b51269250dfd3923ef915bc8f1ec59fd108
pdaniel37 <pedrodaniel@candev.com.br>
2019-04-12 23:07:23 -0300

    Addingplaylist's endpoint GET
    Co-authored-by: João Victor <joao.bisinotti@candev.com.br>

--- src/routes/playlists.js
+++ src/routes/playlists.js
@@ -16,12 +16,38 @@ spotifyApi.getMe()
         console.log('Something went wrong! - getMe', err);
 });
 
+var playlists = {};
+
+//Retorna um JSON com as playlists colaborativas do usuário
+router.get('/', function(req, res, next) {
+    spotifyApi.getUserPlaylists(userId)
+  .then(function(data) {
+    playlists = data.body.items
+    playlists.toString()
+    var filtered = playlists.filter(function (i,n){
+        if(i.collaborative == true){
+            return true;
+        }
+        return false;
+    });
+    res.json(filtered)
+  },function(err) {
+    console.log('Something went wrong!', err);
+  });
+});
+
+//Seleciona uma playlist colaborativa
+router.post('/', function(req, res) {
+    var id = req.param.id;
+});
+
 router.get('/post', function(req, res, next) {
-    request.post('http://localhost:3000/api/v1/playlists/', {form:{id:userId, name:'New Playlist', description:'New Playlist´s description'}})
+    request.post('http://localhost:3000/api/v1/playlists/post', {form:{id:userId, name:'New Playlist', description:'New Playlist´s description'}})
     res.redirect('/')
 });
 
-router.post('/', function(req, res) {
+//Cria uma playlist colaborativa com um nome(obrigatório) e descrição(opcional)
+router.post('/post', function(req, res) {
     spotifyApi.createPlaylist(req.body.id, req.body.name, { 'public' : false, 'collaborative' : true, 'description' : req.body.description })
       .then(function(data) {
         console.log('Playlist created!')
