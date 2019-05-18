d12cebe5c153a1aee03fb0fe27fd9ec1570f7309
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-05-17 23:14:59 -0300

    Update musicPriority.js
    Add comments, correction in order of parameters and refactoring of indentation.
    
    Co-authored-by: @nicaloribeiro

--- src/routes/musicPriority.js
+++ src/routes/musicPriority.js
@@ -10,41 +10,47 @@ db.useBasicAuth('root', process.env.ARANGO_ROOT_PASSWORD);
 db.useDatabase(process.env.ARANGO_DATABASE);
 
 const objectRequestPut = Joi.object().keys({
-  range_start: Joi.number().integer().required(),
-  range_length: Joi.number().integer(),
-  insert_before: Joi.number().integer().required()
+range_start: Joi.number().integer().required(),
+range_length: Joi.number().integer(),
+insert_before: Joi.number().integer().required()
 });
 
 const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.CLIENT_ID || '',
-  clientSecret: process.env.CLIENT_SECRET || '',
-  redirectUri: `${process.env.HOST_API || ''}/playlists/callback`, //verificar isso
+clientId: process.env.CLIENT_ID || '',
+clientSecret: process.env.CLIENT_SECRET || '',
+redirectUri: `${process.env.HOST_API || ''}/playlists/callback`, //verificar isso
 });
 
 const accessToken = process.env.ACCESS_TOKEN || '';
 spotifyApi.setAccessToken(accessToken);
 
-
+//Salvar e ou atualizar no BD
 router.put('/playlists/:playlistId', async (req, res) => {
-  Joi.validate(req.body, objectRequestPut, (err) => {
+Joi.validate(req.body, objectRequestPut, (err) => {
     if (err) {
-      res.status(400).json(err);
-      throw err;
+        res.status(400).json(err);
+        throw err;
     }
-  });
-  const { playlistId } = req.params;
-  const { range_start, range_length, insert_before } = req.body;
-  spotifyApi.getPlaylist(playlistId)
+});
+
+//Playlist
+const { playlistId } = req.params;
+const { 
+    range_start,//posicao da musica
+    range_length,//quantas musicas que serão reordenadas
+    insert_before //posicao que as músicas serão inseridas
+        } = req.body;
+//começo da playlist
+spotifyApi.getPlaylist(playlistId)
     .then(
-      async (data) => {
-        spotifyApi.reorderTracksInPlaylist(playlistId, range_start, range_length, insert_before)
-          .then(function (data) {
-            console.log('Músicas Reordenadas');
-          }, function (err) {
-            console.log('Erro ao Reordenar', err);
-          });
-      })
+        async (data) => {
+            spotifyApi.reorderTracksInPlaylist(playlistId, range_start, insert_before, range_length)
+                .then(function (data) {
+                    console.log('Foi reordenada a playlist!');
+                }, function (err) {
+                    console.log('Houve um erro na reordenação da playlist!', err);
+                });
+        })
 })
 
 module.exports = router
-
